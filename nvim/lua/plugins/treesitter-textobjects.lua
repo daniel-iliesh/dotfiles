d183034ef5c6b1d53f0e85f2195224199850505c
Daniel Ilies <iliesh.daniel@gmail.com>
2025-03-21 22:38:24 +0400

    fixed errors

--- nvim/lua/plugins/treesitter-textobjects.lua
+++ nvim/lua/plugins/treesitter-textobjects.lua
@@ -1,24 +1,30 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
-  event = "VeryLazy",
+  event = "VeryLazy",  -- or another standard event like "BufReadPost"
   enabled = true,
   config = function()
-    -- If treesitter is already loaded, we need to run config again for textobjects
-    if LazyVim.is_loaded("nvim-treesitter") then
-      local opts = LazyVim.opts("nvim-treesitter")
-      require("nvim-treesitter.configs").setup({ textobjects = opts.textobjects })
-    end
+    local textobject_opts = {
+      select = {
+        enable = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          -- add other mappings here
+        },
+      },
+      -- add additional textobjects configuration as needed
+    }
+    require("nvim-treesitter.configs").setup({ textobjects = textobject_opts })
 
-    -- When in diff mode, we want to use the default
-    -- vim text objects c & C instead of the treesitter ones.
-    local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+    -- Optionally, patch the move module as in your original code
+    local move = require("nvim-treesitter.textobjects.move")
     local configs = require("nvim-treesitter.configs")
     for name, fn in pairs(move) do
       if name:find("goto") == 1 then
         move[name] = function(q, ...)
           if vim.wo.diff then
-            local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
-            for key, query in pairs(config or {}) do
+            local config = configs.get_module("textobjects.move")[name] or {}
+            for key, query in pairs(config) do
               if q == query and key:find("[%]%[][cC]") then
                 vim.cmd("normal! " .. key)
                 return
@@ -31,3 +37,4 @@ return {
     end
   end,
 }
+

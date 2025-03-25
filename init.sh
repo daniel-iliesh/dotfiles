12c7c3ded198ed1d7009e2da34da810d1f8c8b8e
Daniel Ilies <iliesh.daniel@gmail.com>
2025-03-25 19:32:15 +0400

    Added fonts, scripts and p10k theme config

--- init.sh
+++ init.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# Dotfiles
 ln -sfn ~/.dotfiles/nvim ~/.config/nvim
 ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -sfn ~/.dotfiles/.tmux.conf ~/.tmux.conf 
 ln -sfn ~/.dotfiles/.oh-my-zsh ~/.oh-my-zsh 
+ln -sfn ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# Scripts
+ln -sfn ~/.dotfiles/scripts/zpm ~/.local/bin/zpm
+
+# Fonts
+ln -sfn ~/.dotfiles/fonts/FiraCodeNerdFont ~/.fonts/FiraCodeNerdFont
+fc-cache -fv
 
 echo "Dotfiles linked successfully!"
--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
 sudo apt install fzf
 sudo apt install fd-find
 sudo apt install ripgrep
+
+zpm install jeffreytse/zsh-vi-mode
+zpm install zsh-users/zsh-syntax-highlighting
+zpm install zsh-users/zsh-autosuggestions
+
+zpm install --no-enable romkatv/powerlevel10k
